474dbcc2f32eed5a21147030224ca394590c9348
ChrisKusi <kusichris656@gmail.com>
2023-03-28 10:37:10 +0100

    Updated commit

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO #import YOLO algorithm from ultralyrics
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 #model = YOLO("yolov8n.pt")  # build a pre-trained model from scratch
 # Use the model
-results = model.train(data="D:\YOLOv8 Local\Brain_Tumor_Detection\config.yaml", batch=60, epochs=50, name='brain_tumor_custom_#1')  # train the model
+results = model.train(data="D:\YOLOv8 Local\Brain_Tumor_Detection\config.yaml", batch=16, epochs=100, name='brain_tumor_custom_#4')  # train the model
