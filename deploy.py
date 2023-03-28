c1a21eb4f8ee410d079a8e8e3e16e0fb5d556c22
ChrisKusi <kusichris656@gmail.com>
2023-03-28 10:12:45 +0100

    Updated commit

--- deploy.py
+++ deploy.py
@@ -109,7 +109,9 @@ if option == "Real-time Detection":
 
 
 
-    webrtc_streamer(key="example", video_frame_callback=callback)
+    webrtc_streamer(key="example", video_frame_callback=callback, rtc_configuration={  # Add this line
+    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+        
 
 
 
